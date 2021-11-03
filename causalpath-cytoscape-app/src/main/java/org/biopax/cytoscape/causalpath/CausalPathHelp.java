a8c5399dd4e8f21654d243a87513dc51b47cc4d5
Pritam Saha <34870686+thepritam@users.noreply.github.com>
2021-11-03 11:45:50 +0530

    Changed Made

--- causalpath-cytoscape-app/src/main/java/org/biopax/cytoscape/causalpath/CausalPathHelp.java
+++ causalpath-cytoscape-app/src/main/java/org/biopax/cytoscape/causalpath/CausalPathHelp.java
@@ -10,7 +10,7 @@ public class CausalPathHelp extends javax.swing.JFrame {
     public CausalPathHelp() {
         initComponents();
     }
-
+    
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
